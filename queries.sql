4e54f5504d50a592dc5885ec72d43082c5bf39b4
Ratika <ratika050190@outlook.com>
2021-09-17 22:22:04 -0500

    Added python notebook and SQL Script -Ratika

---
+++ queries.sql
@@ -0,0 +1,7 @@
+select * from video_category;
+
+select count(id) from video_category;
+
+select * from us_videos LIMIT 5;
+
+select count(id) from us_videos;
---
+++ table_schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE video_category(
+id INT PRIMARY KEY,
+category_title VARCHAR
+)
+
+
+--Create table for US Youtube Videos
+CREATE TABLE us_videos (
+	id SERIAL PRIMARY KEY,
+	video_id VARCHAR NOT NULL,
+	title VARCHAR,
+	description VARCHAR,
+	channel_title VARCHAR,
+	category_id INT NOT NULL,
+	view_count INT,
+	like_count INT,
+	dislike_count INT,
+	comment_Count INT,
+	trending_date TIMESTAMP,
+	trending_month INT,
+	trending_year INT,
+	publish_date TIMESTAMP,
+	publish_month INT,
+	publish_year INT,
+	FOREIGN KEY (category_id) REFERENCES video_category(id)
+);
